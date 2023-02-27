426bd225e3bc136db2e6a7134669d4223d53ae13
puzzledmonkey <chris@oedo.net>
2023-02-27 11:39:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AWS ECS Stop Task
+
+Stops a running task on AWS ECS.
+
+## License Summary
+
+This code is made available under the MIT license.
---
+++ index.js
@@ -0,0 +1,64 @@
+const core = require('@actions/core');
+const AWS = require('aws-sdk');
+
+const ecs = new AWS.ECS();
+
+const main = async () => {
+  const cluster = core.getInput('cluster', { required: true });
+  const service = core.getInput('service', { required: true });
+  const group = core.getInput('group', { required: true });
+  if (group == 'service') {
+    throw new Error(`Cannot stop a group called 'service'`);
+  }
+
+  try {
+    // Get network configuration from aws directly from describe services
+    core.debug('Getting information from service');
+    const info = await ecs
+      .describeServices({ cluster, services: [service] })
+      .promise();
+
+    if (!info || !info.services[0]) {
+      throw new Error(
+        `Could not find service ${service} in cluster ${cluster}`
+      );
+    }
+
+    const taskDefinition = info.services[0].taskDefinition;
+    const taskDefinitionName = taskDefinition
+      .split('/')
+      .pop()
+      .split(':')
+      .shift();
+    // core.setOutput('task-definition', taskDefinition);
+
+    const existingARNs = await ecs
+      .listTasks({ cluster, family: taskDefinitionName })
+      .promise();
+    if (
+      existingARNs &&
+      existingARNs.taskArns &&
+      existingARNs.taskArns.length > 0
+    ) {
+      const existing = await ecs
+        .describeTasks({
+          cluster,
+          tasks: existingARNs.taskArns,
+        })
+        .promise();
+      if (existing && existing.tasks) {
+        const tasksIds = existing.tasks
+          .filter((t) => t.group == name + ':' + service)
+          .map((t) => t.taskArn.split('/').pop());
+        for (let i = 0; i < tasksIds.length; i++) {
+          core.info('Stopping task ID ' + tasksIds[i]);
+          await ecs.stopTask({ cluster, task: tasksIds[i] }).promise();
+        }
+      }
+    }
+  } catch (error) {
+    core.setFailed(error);
+  }
+};
+
+main();
